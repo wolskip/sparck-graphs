821567c7fda7af9ab22cecd9434d4b3b7314eb83
Jan Kaspar <Jan.Kaspar@bjss.com>
2016-06-21 17:11:09 +0100

    brexit quick fixes + slider

--- brexit/brexit.js
+++ brexit/brexit.js
@@ -1,37 +1,7 @@
 (function(){
-  var containerGraph1 = '.chart-graph';
-  var containerGraph2 = '.chart-graph2';
+  var containerGraph1 = '.count-chart';
+  var containerGraph2 = '.percentage-chart';
   var labels = [
-    {
-      "start": new Date(2016, 5, 13, 17, 45, 0, 0),
-      "end": new Date(2016, 5, 13, 17, 55, 0, 0),
-      "title": "Expectations were high, but sentiment dropped after the event started.",
-      "text": "Right up until 6PM, a significant positive spike in the way people were talking about WWDC was apparent. There was a real sense of excitement, largely provoked by months of rumours about what was about to drop. However, when the event started, sentiment peaked, and took a steady decline right up until it moved into MacOS and iOS territory. "
-    },
-    {
-      "start": new Date(2016, 5, 13, 18, 27, 0, 0),
-      "end": new Date(2016, 5, 13, 18, 36, 0, 0),
-      "title": "TVOS just didn’t go down well.",
-      "text": "This whole section of the Keynote fared pretty badly: Twitter was alive with tweets describing the presenter as patronising and boring, and the new features as deeply uninspiring."
-    },
-    {
-      "start": new Date(2016, 5, 13, 18, 42, 0, 0),
-      "end": new Date(2016, 5, 13, 19, 7, 0, 0),
-      "title": "iOS was the most popular update. ",
-      "text": "This should come as no surprise, but people found the iOS section of the Keynote the most engaging. Pundits were overjoyed to hear about new emoji and updates to Apple Music - but it was clear from both the audience reaction and SPARCK analysis that developers being asked to ‘rap along to their favourite tunes’ was an unpopular request."
-    },
-    {
-      "start": new Date(2016, 5, 13, 19, 15, 0, 0),
-      "end": new Date(2016, 5, 13, 19, 23, 0, 0),
-      "title": "HomeKit, Messages and new features in the Phone app went down a treat.",
-      "text": "New features in iOS let you see who’s at your door intercom from your phone, see your voicemail messages as text, and a whole host of new messaging features such as emoji, live backgrounds and stickers were really well received."
-    },
-    {
-      "start": new Date(2016, 5, 13, 19, 38, 0, 0),
-      "end": new Date(2016, 5, 13, 20, 20, 0, 0),
-      "title": "Kids learning to code was popular amongst the developer community. ",
-      "text": "Apple's announcement to offer a free learning tool for kids to learn their programming language, Swift, could have been met with some cynicism. However, SPARCK analysis shows that this news was warmly welcomed."
-    }
   ];
 
   function jsonWithRetry(url, retries, success){
@@ -55,8 +25,15 @@
 drawGraph(containerGraph1);
 drawGraph(containerGraph2, updateToPercentage, function(n){ return n + "%"; });
 
+
+var rawData;
+
+
+
 function drawGraph(containerGraph, updateData, tickFormat)
 {
+  var loaded;
+
   // Set the dimensions of the canvas / graph
   var margin = {top: 30, right: 20, bottom: 30, left: 50},
       width = 900 - margin.left - margin.right,
@@ -75,7 +52,7 @@ function drawGraph(containerGraph, updateData, tickFormat)
   var yAxis = d3.svg.axis().scale(y)
               .orient("left")
               .ticks(6);
-              
+
     if (tickFormat != undefined)
     {
         yAxis.tickFormat(tickFormat);
@@ -105,40 +82,42 @@ function drawGraph(containerGraph, updateData, tickFormat)
         .append("rect")
         .attr("width", width)
         .attr("height", height);
-   
-    
+
+
   var desc = d3.select(containerGraph).append("div")
     .attr("class", "graph-description");
 
     desc.append("h4");
     desc.append("p");
-    
+
   var data;
   var zoom = d3.behavior.zoom();
   var maxDate, minDate;
 
   function loadGraph()
   {
+      loaded = true;
+
       maxDate = d3.min(data.time);
       minDate = d3.max(data.time);
-            
-     var length = data.time.length;
-      
+
+      var length = data.time.length;
+
       // Scale the range of the data
-      x.domain([maxDate, minDate]);      
+      x.domain([maxDate, minDate]);
       y.domain([d3.min([d3.min(data.remain), d3.min(data.leave)]) - 4, d3.max([d3.max(data.remain), d3.max(data.leave)]) + 4]);
 
       zoom.x(x);
 
       var thick = 1;
-      
+
         if (tickFormat != undefined)
         {
             svg.append("path")
             .attr("class", "xaxis")
             .style('stroke-width', thick)
             .attr("d", line([[0,y(50)],[width, y(50)]]));
-        } 
+        }
 
         svg.append("clipPath")
             .attr("id", "clip-all")
@@ -176,7 +155,7 @@ function drawGraph(containerGraph, updateData, tickFormat)
         //       .attr("class", "label-bottom")
         //       .attr("clip-path", "url(#clip-all)")
         //       .attr("d", line([[0,0], [0,0]]));
-              
+
         //updateLabels();
 
         // Add the X Axis
@@ -249,19 +228,19 @@ function drawGraph(containerGraph, updateData, tickFormat)
          function mousezoom(){
 
             var currentTranstate = zoom.translate();
-                                
+
             var length = x(minDate)-  x(maxDate);
             var min = -(length - width);
             //console.log("min",min, length);
-            //console.log("currentTranstate", currentTranstate[0])    
-            
+            //console.log("currentTranstate", currentTranstate[0])
+
             if(currentTranstate[0] > 0){
               zoom.translate([0, currentTranstate[1]]);
             }
-            
+
             if(currentTranstate[0] < min){
                 zoom.translate([min, currentTranstate[1]]);
-            }            
+            }
             redraw();
          }
   }
@@ -309,35 +288,74 @@ function drawGraph(containerGraph, updateData, tickFormat)
   {
       jsonWithRetry("https://ps4ez07vul.execute-api.eu-west-1.amazonaws.com/v1/brexit/graph", 3, function(json) {
           data = json;
-          
+
           if (updateData != undefined)
           {
             updateData(data);
           }
           parseTimeValues(data);
-          loadGraph();
+
+          // todo change - super dirty caches first graf data load...
+          if(!updateData){
+            rawData = data;
+            createSlider();
+          }
+
+          // todo: update will break it :)
+          if(loaded) {
+            redraw()
+          } else {
+            loadGraph();
+          }
       });
   }
-  
-  setInterval(loadPastData, 1800000);
-  
+
+  //setInterval(loadPastData, 1800000);
+
   function parseTimeValues()
   {
       data.time.forEach(function(element, i, arr) {
           arr[i] = moment(element)._d;
       });
-  } 
-  
- 
+  }
 }
 
 function updateToPercentage(data)
 {
     for(i=0; i< data.leave.length; i++)
     {
-          data.leave[i] = 100 * data.leave[i] / (data.leave[i] + data.remain[i]);  
-          data.remain[i] = 100 - data.leave[i];  
+          data.leave[i] = 100 * data.leave[i] / (data.leave[i] + data.remain[i]);
+          data.remain[i] = 100 - data.leave[i];
     }
-} 
+}
+
+
+function createSlider(){
+
+   var remain = rawData.remain[rawData.remain.length - 1];
+   var leave = rawData.leave[rawData.leave.length - 1];
+
+   var remainP = remain / (remain + leave) * 100;
+
+   var container = d3.select('.slider-chart');
+   var slider = container.append('div')
+      .attr('class', 'slider')
+
+  slider.append('div')
+      .attr('class', 'slider-remain')
+      .style('width', remainP + '%')
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 })();
